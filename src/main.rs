38af193dfb94c4d3ccf573c833f5a19673b69c1b
Wolfgang Pupp <pupp@vemcon.de>
2023-05-26 21:16:20 +0200

    SEtting outputs with PIO

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
 use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
@@ -17,10 +16,16 @@ use rp_pico as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
+    gpio::{FunctionPio0, Pin},
     pac,
+    pio::PIOExt,
     sio::Sio,
     watchdog::Watchdog,
 };
+
+fn add_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
 
 #[entry]
 fn main() -> ! {
@@ -53,6 +58,39 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
+    // configure LED pin for Pio0.
+    let _led: Pin<_, FunctionPio0> = pins.gpio15.into_mode();
+    // PIN id for use inside of PIO
+    let led_pin_id = add_numbers(15, 0) as u8;
+
+    // Define some simple PIO program.
+    let mut a = pio::Assembler::<32>::new();
+    let mut wrap_target = a.label();
+    let mut wrap_source = a.label();
+
+    // Set pin as Out
+    a.set(pio::SetDestination::PINDIRS, 1);
+    // Define begin of program loop
+    a.bind(&mut wrap_target);
+    a.set(pio::SetDestination::PINS, 0);
+    a.set(pio::SetDestination::PINS, 1);
+    a.bind(&mut wrap_source);
+
+    // The labels wrap_target and wrap_source, as set above,
+    // define a loop which is executed repeatedly by the PIO
+    // state machine.
+    let program = a.assemble_with_wrap(wrap_source, wrap_target);
+
+    // Initialize and start PIO
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let installed = pio.install(&program).unwrap();
+    let (int, frac) = (1, 1); // as slow as possible (0 is interpreted as 65536)
+    let (sm, _, _) = rp2040_hal::pio::PIOBuilder::from_program(installed)
+        .set_pins(led_pin_id, 1)
+        .clock_divisor_fixed_point(int, frac)
+        .build(sm0);
+    sm.start();
+
     // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
     // on-board LED, it might need to be changed.
     // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
@@ -60,14 +98,14 @@ fn main() -> ! {
     // LED to one of the GPIO pins, and reference that pin here.
     // let mut led_pin = pins.led.into_push_pull_output();
 
-    let mut led_pin = pins.gpio14.into_push_pull_output(); //led.into_push_pull_output();
+    // let mut led_pin = pins.gpio14.into_push_pull_output(); //led.into_push_pull_output();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
+        // info!("on!");
+        // led_pin.set_high().unwrap();
+        // delay.delay_ms(500);
+        // info!("off!");
+        // led_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
 }
